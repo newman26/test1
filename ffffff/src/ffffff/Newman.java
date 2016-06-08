2b82658aa0cc1bdf30596bb583a9b0631367bb85
nomane <nomane.boulmerdj@gmail.com>
2016-06-08 22:11:50 +0200

    commit 2 Newman class

--- ffffff/src/ffffff/Newman.java
+++ ffffff/src/ffffff/Newman.java
@@ -3,6 +3,14 @@ package ffffff;
 public class Newman {
 private int sss;
 private String ddd;
+
+
+
+public Newman(int sss, String ddd) {
+	super();
+	this.sss = sss;
+	this.ddd = ddd;
+}
 public int getSss() {
 	return sss;
 }
