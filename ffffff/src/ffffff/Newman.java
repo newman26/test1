4816571bde512a3ccf5215c70c015583cc11eef0
nomane <nomane.boulmerdj@gmail.com>
2016-06-08 21:59:20 +0200

    commit class newman

---
+++ ffffff/src/ffffff/Newman.java
@@ -0,0 +1,19 @@
+package ffffff;
+
+public class Newman {
+private int sss;
+private String ddd;
+public int getSss() {
+	return sss;
+}
+public void setSss(int sss) {
+	this.sss = sss;
+}
+public String getDdd() {
+	return ddd;
+}
+public void setDdd(String ddd) {
+	this.ddd = ddd;
+}
+ 
+}
